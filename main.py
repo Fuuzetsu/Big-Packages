b9d7e06f199582867d00cc2b5c2c92629778a9ec
Mateusz Kowalczyk <mk440@bath.ac.uk>
2012-10-05 22:42:13 +0100

    A friendlier MUSIC loader for the dev. Cut under 80 columns.

--- main.py
+++ main.py
@@ -13,9 +13,13 @@ import os.path
 # Joins a list of paths into a single path (OS independent)
 list_pj = lambda l: reduce(os.path.join, l) 
 
+# Composition of join and load
+load_join_i = lambda l: pyglet.image.load(list_pj(l))
+load_join_m = lambda l: pyglet.media.load(list_pj(l))
+
 def resourceInit():
     try: #This will install avbin if needed, and it works on windows and linux!
-        pyglet.resource.image(list_pj(["resources", "art", "background1.png"]))
+        load_join_i(["resources", "art", "background1.png"])
     except:
         if sys.platform.startswith("win"):
             print "Error: avbin.dll not found."
@@ -35,15 +39,20 @@ def resourceInit():
 
 class Game(pyglet.window.Window):
     def __init__(self, width, height):
-        self.MUSIC = {"JingleBellsA" : pyglet.media.load(list_pj(["resources", "music", "Jingle Bells.mp3"])),
-                      "JingleBellsB" : pyglet.media.load(list_pj(["resources", "music", "Jingle Bells 3.mp3"])),
-                      "OhChristmasTree" : pyglet.media.load(list_pj(["resources", "music", "Oh Xmas.mp3"])),
-                      "UpOnAHouseTop" : pyglet.media.load(list_pj(["resources", "music", "Up on a Housetop.mp3"])),
-                      "WeWishYou" : pyglet.media.load(list_pj(["resources", "music", "We Wish You.mp3"])),
-                      "Grinch1" : pyglet.media.load(list_pj(["resources", "music", "grinch.mp3"]))
-                      # Grinch2 refuses to load, comment out for now
-                      #"Grinch2" : pyglet.media.load(list_pj(["resources", "music", "02-Grinch.mp3)"]))
-                      }
+        self.MUSIC = { 
+            "JingleBellsA"    : ["resources", "music", "Jingle Bells.mp3"],
+            "JingleBellsB"    : ["resources", "music", "Jingle Bells 3.mp3"],
+            "OhChristmasTree" : ["resources", "music", "Oh Xmas.mp3"],
+            "UpOnAHouseTop"   : ["resources", "music", "Up on a Housetop.mp3"],
+            "WeWishYou"       : ["resources", "music", "We Wish You.mp3"],
+            "Grinch1"         : ["resources", "music", "grinch.mp3"]
+            # Grinch2 refuses to load, comment out for now
+            #"Grinch2"         : ["resources", "music", "02-Grinch.mp3)"]))
+            }
+        
+        for k, v in self.MUSIC.iteritems(): 
+            self.MUSIC[k] = load_join_m(v)
+
         messenger.Messenger.game = self
         self.create_music_player()
         self.fps_display = pyglet.clock.ClockDisplay()
@@ -63,23 +72,25 @@ class Game(pyglet.window.Window):
         #TESTING PURPOSES
 
     def create_music_player(self):
-        """Creates a music player and loads all the fun christmas music on it."""
+        """Creates a music player and loads all the fun christmas music."""
         self.music_player = pyglet.media.Player()
         #self.music_player.eos_action = "loop" #disabling looping
+
         music_list = []
-        for key in self.MUSIC:
-            item = self.MUSIC[key]
-            music_list.append(item)
+        for song in self.MUSIC.values():
+            music_list.append(song)
+
         random.shuffle(music_list)
         for song in music_list:
             self.music_player.queue(song)
+
         self.music_player.play()
         pyglet.clock.schedule_interval(lambda _: self.music_player.play(), 2.0)
 
     def on_draw(self):
         self.clear()
         self.mode.on_draw()
-        #self.fps_display.draw() #FPS should not be displayed during normal gameplay
+        #self.fps_display.draw() # Hide FPS during regular play
 
     def on_key_press(self, symbol, modifiers):
         self.mode.on_key_press(symbol, modifiers)
